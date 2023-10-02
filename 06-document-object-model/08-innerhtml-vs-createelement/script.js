aed4abff00b38a277b35da1db5ac343ce7adbeca
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-02 14:45:14 +0800

    JS Lectures

---
+++ 06-document-object-model/08-innerhtml-vs-createelement/script.js
@@ -0,0 +1,45 @@
+//Quick and Dirty
+function createListItem(item) {
+  const li = document.createElement('li');
+
+  li.innerHTML = ` ${item}
+  <button class="remove-item btn-link text-red">
+    <i class="fa-solid fa-xmark"></i>
+  </button>`;
+
+  document.querySelector('.items').appendChild(li);
+}
+
+//Clean & Performant
+function createNewItem(item) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement('button');
+  button.className = 'remove-item btn-link text-red';
+
+  const icon = document.createElement('i');
+  icon.className = 'fa-solid fa-xmark';
+
+  button.appendChild(icon);
+  li.appendChild(button);
+
+  document.querySelector('.items').appendChild(li);
+}
+
+createListItem('Cloud 9');
+createNewItem('Sugo');
+
+//If you will still refactor:
+
+// function createAssignmentItem(item) {
+
+// }
+
+// function createButton(classes) {
+
+// }
+
+// function createIcon(classes) {
+
+// }
--- 06-document-object-model/09-insert-elements/script.js
+++ 06-document-object-model/09-insert-elements/script.js
@@ -1,10 +1,43 @@
 //insertAdjacentElement Example
+function insertElement() {
+  const filter = document.querySelector('.filter');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'insertAdjacentElement';
+
+  filter.insertAdjacentElement('afterend', h1);
+}
+insertElement();
 
 //insertAdjacentText Example
+function insertText() {
+  const item = document.querySelector('li:first-child');
 
+  item.insertAdjacentText('afterend', 'insertAdjacentText');
+}
+insertText();
 //insertAdjacentHTML example
+function insertHTML() {
+  const clearBtn = document.querySelector('#clear');
 
-//indesrt Before Example
+  clearBtn.insertAdjacentHTML('afterend', '<h2>insertAdjacentHTML</h2>');
+}
+insertHTML();
+
+//insert Before Example
+
+function insertBeforeItem() {
+  const ul = document.querySelector('ul');
+
+  const li = document.createElement('li');
+  li.textContent = 'insertBefore';
+
+  const thirdItem = document.querySelector('li:nth-child(3)');
+
+  ul.insertBefore(li, thirdItem);
+}
+
+insertBeforeItem();
 
 //#######################################
 /* <!-- beforebegin -->
