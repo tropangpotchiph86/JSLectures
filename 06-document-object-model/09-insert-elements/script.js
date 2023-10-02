b6f7bdc6a2f4d87de090fb0528a3e30b88b1e26d
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-02 08:56:50 +0800

    JS Lectures

---
+++ 06-document-object-model/09-insert-elements/script.js
@@ -0,0 +1,19 @@
+//insertAdjacentElement Example
+
+//insertAdjacentText Example
+
+//insertAdjacentHTML example
+
+//indesrt Before Example
+
+//#######################################
+/* <!-- beforebegin -->
+<p>
+<!-- afterbegin -->
+
+    //foo
+
+    <!-- before end -->
+</p>
+<!-- afterend -->
+ */
