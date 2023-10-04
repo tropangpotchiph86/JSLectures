8a60b21aa017ebada2fd1ac6b6099a9d29f5d285
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-04 09:03:05 +0800

    JS Lectures

--- 06-document-object-model/08-innerhtml-vs-createelement/script.js
+++ 06-document-object-model/08-innerhtml-vs-createelement/script.js
@@ -1,45 +1,63 @@
-//Quick and Dirty
-function createListItem(item) {
-  const li = document.createElement('li');
+// //Quick and Dirty
+// function createListItem(item) {
+//   const li = document.createElement('li');
 
-  li.innerHTML = ` ${item}
-  <button class="remove-item btn-link text-red">
-    <i class="fa-solid fa-xmark"></i>
-  </button>`;
+//   li.innerHTML = ` ${item}
+//   <button class="remove-item btn-link text-red">
+//     <i class="fa-solid fa-xmark"></i>
+//   </button>`;
 
-  document.querySelector('.items').appendChild(li);
-}
+//   document.querySelector('.items').appendChild(li);
+// }
 
-//Clean & Performant
+// //Clean & Performant
+// function createNewItem(item) {
+//   const li = document.createElement('li');
+//   li.appendChild(document.createTextNode(item));
+
+//   const button = document.createElement('button');
+//   button.className = 'remove-item btn-link text-red';
+
+//   const icon = document.createElement('i');
+//   icon.className = 'fa-solid fa-xmark';
+
+//   button.appendChild(icon);
+//   li.appendChild(button);
+
+//   document.querySelector('.items').appendChild(li);
+// }
+
+// createListItem('Cloud 9');
+// createNewItem('Sugo');
+
+// //refactored code
+
 function createNewItem(item) {
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(item));
 
-  const button = document.createElement('button');
-  button.className = 'remove-item btn-link text-red';
+  const button = createButton('remove-item btn-link text-red');
 
-  const icon = document.createElement('i');
-  icon.className = 'fa-solid fa-xmark';
-
-  button.appendChild(icon);
   li.appendChild(button);
 
   document.querySelector('.items').appendChild(li);
 }
 
-createListItem('Cloud 9');
-createNewItem('Sugo');
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
 
-//If you will still refactor:
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
 
-// function createAssignmentItem(item) {
+  return button;
+}
 
-// }
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
 
-// function createButton(classes) {
-
-// }
-
-// function createIcon(classes) {
-
-// }
+createNewItem('Cheese');
+createNewItem('Sauce');
