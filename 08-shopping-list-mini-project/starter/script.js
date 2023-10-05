4263264e2376ec7c7ee8972eb549bec8f9294f08
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-05 13:20:06 +0800

    JS Lectures

---
+++ 08-shopping-list-mini-project/starter/script.js
@@ -0,0 +1,9 @@
+//########################################
+//########################################
+// localStorage.setItem('name', 'Ryan');
+
+// console.log(localStorage.getItem('name'));
+
+// // localStorage.removeItem('name');
+
+// localStorage.clear();
