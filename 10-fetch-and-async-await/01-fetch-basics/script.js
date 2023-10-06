22819398d217508ebd79445431d153e4ab4a63d5
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-06 14:29:13 +0800

    JS Lectures

--- 10-fetch-and-async-await/01-fetch-basics/script.js
+++ 10-fetch-and-async-await/01-fetch-basics/script.js
@@ -4,16 +4,35 @@
 
 //##################################################
 //Fetch a JSON file
-fetch('./movies.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// fetch('./movies.json')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
 
 //##################################################
+//refactoring
+fetch('./movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
 //##################################################
+//Fetching a text
+fetch('./test.txt')
+  .then((response) => response.text())
+  .then((data) => console.log(data));
 //##################################################
+//Fetching from an API
+fetch('https://api.github.com/users')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 //##################################################
+fetch('https://api.github.com/users/tropangpotchiph86')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 //##################################################
+fetch('https://api.github.com/users/tropangpotchiph86')
+  .then((response) => response.json())
+  .then((data) => (document.querySelector('h1').textContent = data.login));
---
+++ 10-fetch-and-async-await/03-fetch-options/script.js
@@ -0,0 +1,17 @@
+function createPost(title, body) {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      token: 'wd86passcode',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+createPost({ title: 'My Post', body: 'This is wd86 post' });
